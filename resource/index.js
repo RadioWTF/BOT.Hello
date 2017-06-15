9032b40773234641963e22bb7fd7fbbd32a06897
Kitsune Solar <kitsune.solar@gmail.com>
2017-06-15 17:05:24 +0300

    Update resource

--- resource/index.js
+++ resource/index.js
@@ -20,16 +20,16 @@ bot.on('ready', () => {
 bot.on('guildMemberAdd', member => {
 	const embed = new Discord.RichEmbed()
 		.setTitle('Сообщество FURRY')
-		.setAuthor('FURS.Life', 'https://cdn.discordapp.com/avatars/317570687629918208/5f0811c02dbbdf4903215874841d17e7.jpg')
+		.setAuthor('FURS.Network', 'https://cdn.discordapp.com/avatars/317570687629918208/5f0811c02dbbdf4903215874841d17e7.jpg')
 		.setColor(0xFF0000)
-		.setDescription('Привет, ' + member + '!\nЯ бот сообщества [FURS.Life](https://furs.life/) и интернет-радиостанции [Радио W.T.F.](https://radio.wtf/)\nУ нас ты можешь найти друзей по интересам, узнать много нового и необычного, рассказать о своем увлечении или навыке в прямом эфире.')
-		.setFooter('© FURS.Life','https://cdn.discordapp.com/avatars/317570687629918208/5f0811c02dbbdf4903215874841d17e7.jpg')
+		.setDescription('Привет, ' + member + '!\nЯ бот сообщества [FURS.Network](https://furs.network/) и интернет-радиостанции [Радио W.T.F.](https://radio.wtf/)\nУ нас ты можешь найти друзей по интересам, узнать много нового и необычного, рассказать о своем увлечении или навыке в прямом эфире.')
+		.setFooter('© FURS.Network','https://cdn.discordapp.com/avatars/317570687629918208/5f0811c02dbbdf4903215874841d17e7.jpg')
 		.setImage('')
 		.setThumbnail('')
 		.setTimestamp()
-		.setURL('https://furs.life')
+		.setURL('https://furs.network')
 		.addField('# Настройки','1. Зайди в **Настройки пользователя > Голос > Режим ввода** и включи **Push to Talk**.\n2. Справа назначь кнопку, по нажатии на которую ты будешь говорить.')
-		.addField('# Ресурсы', '- [FURS.Life](https://furs.life/)\n- [Радио W.T.F.](https://radio.wtf/)')
+		.addField('# Ресурсы', '- [FURS.Network](https://furs.network/)\n- [Радио W.T.F.](https://radio.wtf/)')
 		.addField('# Социальные сети', '[ВКонтакте](https://vk.com/radiowtf) | [Facebook](https://www.facebook.com/wtf.space) | [Twitter](https://twitter.com/wtf_space) | [YouTube](https://www.youtube.com/channel/UC_20PUsjh897fA8QkSXqjeg) | [GitHub](https://github.com/RadioWTF)');
 
 	member.guild.defaultChannel.send({embed});
